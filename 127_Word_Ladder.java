ea545bb93a42a41168eab4860b679311c99cfba6
libralmy <libralmy1003@gmail.com>
2016-08-28 02:17:01 -0400

    Update 127_Word_Ladder.java

--- 127_Word_Ladder.java
+++ 127_Word_Ladder.java
@@ -38,3 +38,36 @@ public class Solution {
         return 0;
     }
 }
+
+
+/**
+use hashset reached.add(beginword) 
+new set toAdd is the alll the word been searched,  wordDict is the non reached part
+***/
+
+    public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
+        Set<String> reached = new HashSet<String>();
+        reached.add(beginWord);
+        wordDict.add(endWord);
+        int distance = 1;
+        while(!reached.contains(endWord)) {
+            Set<String> toAdd = new HashSet<String>();
+            for(String each : reached) {
+                for (int i = 0; i < each.length(); i++) {
+                    char[] chars = each.toCharArray();
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        chars[i] = ch;
+                        String word = new String(chars);
+                        if(wordDict.contains(word)) {
+                            toAdd.add(word);
+                            wordDict.remove(word);
+                        }
+                    }
+                }
+            }
+            distance ++;
+            if(toAdd.size() == 0) return 0;
+            reached = toAdd;
+        }
+        return distance;
+    }
