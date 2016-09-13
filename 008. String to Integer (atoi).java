722812f6a18bc652f00df13352bc1802cc8bc8a5
libralmy <libralmy1003@gmail.com>
2016-09-12 22:48:57 -0400

    Update 008. String to Integer (atoi).java

--- 008. String to Integer (atoi).java
+++ 008. String to Integer (atoi).java
@@ -1,54 +1,43 @@
 
     /**
-     * @param str an input string
-     * @return a int that converted by string
-     *
-     * conner case: str == null
-     * long sum
-     * condition: 7
-     *            space :trim() before do anything
-     *            contains non-number: Charactor.isDigit()
-     *            normal: sum = sum*10 + charAt -'0'
-     *            positive sign: keep sign from first char as 1
-     *            negative sign: keep sign from first char as -1
-     *            >MAX_VALUE: return max
-     *            <MIN_VALUE: return min
-     * return (int)sum*sign
-     *
-     *
-     * ***/
+    * @param a given string 
+    * @return convert string to integer
+    * conner case: flag of negative; isDigit; overflow;
+    * trim();
+    * record the first elem is negative sign or not
+    * iterate str.char() from start
+    *   if(!Character.isDigit(temp)) return return (int)sum * sign;
+    *   char - '0' 
+    *   sum*10 + char - '0'
+    *   greater than MAX, smaller than MIN
+    **/
 public class Solution {
-    public static int myAtoi(String str) {
-        if(str.length() == 0){return 0;}
-        long sum = 0;
-        int sign = 1;
-        int start = 0;
+    public int myAtoi(String str) {
+        if(str == null || str.length() == 0){return 0;}
         str = str.trim();
-        
+        long sum = 0; 
+        int start = 0, sign = 1;
         if(str.charAt(0) == '+'){
-            start++;
+            sign = 1;
+            start = 1;
         }else if(str.charAt(0) == '-'){
             sign = -1;
-            start++;
+            start = 1;
         }
-
-        for(int i = start; i < str.length(); i++){
+        
+        for(int i = start ; i < str.length(); i++){
             char temp = str.charAt(i);
-            
             if(!Character.isDigit(temp)){
-                return (int) sum*sign;
+                return (int)sum * sign;
             }
-                
-            sum = sum*10 + (temp - '0');
+            sum = sum*10 + (temp-'0');
             
-            if(sign == 1 && sum > Integer.MAX_VALUE){
+            if(sign == 1 && sum >= Integer.MAX_VALUE){
                 return Integer.MAX_VALUE;
-            }else if(sign == -1 && (sign * sum) < Integer.MIN_VALUE){
+            }else if(sign == -1 && sum * sign <= Integer.MIN_VALUE){
                 return Integer.MIN_VALUE;
             }
         }
-
-        return (int) sum*sign;
-
+        return (int)sum * sign;
     }
 }
