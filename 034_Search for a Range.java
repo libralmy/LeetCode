bc41b6396a9e081c34edbed86a07553e9a6d2a85
libralmy <libralmy1003@gmail.com>
2016-09-13 02:16:05 -0400

    Update 034_Search for a Range.java

--- 034_Search for a Range.java
+++ 034_Search for a Range.java
@@ -1,54 +1,49 @@
 
     /**
-    * @param nums an int array
-    * @param target an int that will be showing up in the nums
-    * @return an array that contains the start and end indice of target
+    * @param nums a given int array
+    * @param target a integer that need to get the range
+    * @return the range of index of target
     * 
-    * it is looking for two indices for the target new int result[2]
+    * sorted array, use wing <- | -> need to get mid
+    * [left, right] 
+    * left index:
+    * if target < [mid] right = mid -1;
+    * right index
+    * if target > [mid] left = mid +1
     * 
-    * fintIndice() -> two pointer start and end <- | -> Binary Search ~O(logn)
-    * leftbound: isLast = false
-    *            nums[mid] <= target if= idx = mid
-    * rightboudn: isLast = true
-    *            nums[mid] >=target if= idx = mid
-    * result[0] = findIndice(nums, target , isLast) 
-    * result[1] = findIndice(nums, target , isLast);
+    * isEnd is the identification between left and right index
+    * helper(left, right, isEnd)
     **/
-public class Solution {    
+public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
-
-        result[0] = findIndice(nums, target, false);
-        result[1] = findIndice(nums, target, true);
+        int[] result = new int[]{-1, -1};
+        if(nums == null || nums.length == 0){return result;}
         
+        result[0] = findIndex(nums, 0, nums.length -1, target, false);
+        result[1] = findIndex(nums, 0, nums.length - 1, target, true);
         return result;
-
     }
     
-    public int findIndice(int[] nums, int target, boolean isLast){
-        int idx = -1;
-        int start = 0;
-        int end = nums.length-1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(isLast){
-                if(nums[mid] <= target){
-                    if(nums[mid] == target) idx = mid;
-                    start = mid + 1;
+    public int findIndex(int[] nums, int left, int right, int target, boolean isEnd){
+        int index = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(isEnd){
+                if(target >= nums[mid]){
+                    if(target == nums[mid]){ index = mid;}
+                    left = mid + 1;
                 }else{
-                    end = mid - 1;
+                    right = mid - 1;
                 }
             }else{
-                if(nums[mid] >= target){
-                    if(nums[mid] == target) idx = mid;
-                    end = mid - 1;
+                if(target <= nums[mid]){
+                    if(target == nums[mid]){index = mid;}
+                    right = mid - 1;
                 }else{
-                    start = mid + 1;
+                    left = mid + 1;
                 }
             }
-
         }
-        return idx;
+        return index;
     }
-
 }
