fbc69fe311623fa585df48a46a1acd8c0bda6266
libralmy <libralmy1003@gmail.com>
2016-09-13 01:48:48 -0400

    Update 033_Search in Rotated Sorted Array.java

--- 033_Search in Rotated Sorted Array.java
+++ 033_Search in Rotated Sorted Array.java
@@ -1,43 +1,42 @@
 
     /**
-    * @param nums an given sorted and rotated array
-    * @param target find the index of this target
-    * @return the index of the target, if dont find it, return -1
-    * 
-    * while(left<=right)
-    * make sure the rotated point is at left or right
-    * if [mid] >= [left] looking into left side
-    *   start <= target < [mid]] right = mid -1
-    *                           
-    * else looking into right side
-    *   [mid] < target <= [right] left = mid + 1
+    * @param nums a given int array
+    * @param target a integer
+    * @return the index of target
+    *
+    * while(left <= right) length == 1
+    * mid = left +(right - left)/2 
+    * [mid] == target return mid
+    * [left] >[mid] fall in left bound <-
+    *    [left] <= target <[mid] right = mid-1
+    * else fall in right bound ->
+    *    [mid]< target <=[right] left = mid+1
     **/
 public class Solution {
     public int search(int[] nums, int target) {
-        int index = -1, left = 0, right = nums.length -1;
-        
+        if(nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length -1;
         while(left <= right){
             int mid = left + (right - left)/2;
             if(nums[mid] == target){
-                index = mid;
+                return mid;
             }
-            
-            if(nums[left] <= nums[mid]){
-                if(target >= nums[left] && nums[mid] > target){
-                    right = mid - 1;
-                }else{
-                    left = mid + 1;
-                }
-            }else{
-                if(target <= nums[right] && nums[mid] < target){
+            //right bound
+            if(nums[mid] < nums[left]){
+                if(nums[mid] < target && nums[right] >= target){
                     left = mid + 1;
                 }else{
                     right = mid - 1;
                 }
+            }else{//left bound
+                if(nums[left] <= target && nums[mid] > target){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
             }
-            
         }
-        return index;
-
+        
+        return -1;
     }
 }
