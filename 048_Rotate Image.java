4e6ad45d74dc1cbb168695f693ccbe11b6926e9a
libralmy <libralmy1003@gmail.com>
2016-09-13 22:13:18 -0400

    Update 048_Rotate Image.java

--- 048_Rotate Image.java
+++ 048_Rotate Image.java
@@ -1,47 +1,40 @@
-
-    
+public class Solution {
     /**
-    * @param matrix a given 2d int array
-    * @return reverse 90degree
+    * @param 2D matrix
+    * @return rotate the matrix by 90 degree 
     * 
-    * symmetrically \ upper left 
-    * [row,col]=> [col, row]
-    * row<n, col<n
+    * if go swift, then all the point needs to be moved, 2mn
+    * so if could make find the axil to do the flip over, it will be mn/4
+    * rotate -180 and reverse -90 => upper right to bottom left, then left to right
+    * -180 degree => col=row
+    * for(row< length )
+    *   for(col=row  col< length )
+    *       swap(m[row][col], m[col][row])
     * 
-    * vertically | left to right
-    * [row,col]=>[row, n-col-1]
-    * row<n/2, col<n
+    * -90 degree
+    * for(row < len)
+    *   for(col < len/2 )
+    *       swap(m[row][col], m[row][len -col-1])
     **/
-public class Solution {    
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        if(n == 0){return;}
-        for(int row = 0; row < n; row++){
-            for(int col = row; col < n; col++){ //col =0, col<row
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){return;}
+        int len = matrix[0].length;
+        
+        for(int row = 0; row < len; row++){
+            for(int col = row; col < len; col++){
                 int temp = matrix[row][col];
                 matrix[row][col] = matrix[col][row];
-                matrix[col][row] =temp;
-                //swap(matrix[row][col], matrix[col][row]);
+                matrix[col][row] = temp;
             }
         }
         
-        for(int row = 0; row < n; row++){
-            for(int col = 0; col < n/2; col++){
+        
+        for(int row = 0; row < len; row++){
+            for(int col = 0; col < len/2; col++){
                 int temp = matrix[row][col];
-                matrix[row][col] = matrix[row][n-col-1];
-                matrix[row][n-col-1] =temp;
-                
-                //swap(matrix[row][col], matrix[row][n-col-1]);
+                matrix[row][col] = matrix[row][len - 1- col];
+                matrix[row][len-1-col] = temp;
             }
         }
-        
     }
-    
-    public void swap(int a, int b){
-        int temp = 0;
-        temp = a;
-        a = b;
-        b = temp;
-    }
-
 }
