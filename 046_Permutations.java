5ba91bbb275e0f98022bc2a3e9890b10d05b9e41
libralmy <libralmy1003@gmail.com>
2016-09-14 00:12:11 -0400

    Update 046_Permutations.java

--- 046_Permutations.java
+++ 046_Permutations.java
@@ -1,64 +1,40 @@
-
-    /**
-    * @param nums a given int array that contains the basic permutation
-    * @return all the permutations as a List<List<>>
-    * 
-    * [1]+[2,3] ,[2]+[1,3], [3]+[1,2]
-    * backtracking
-    * templist for each level, and boolean[] same length as given array for avoid the duplications
-    * stop point: templist.size() == nums.length
-    * 
-    * iteration of nums
-    * if(!used[]) => backtarcking
-    **/
-
 public class Solution {
-    public List<List<Integer>> permute(int[] nums) {  
+    /**
+    * @param nums a given int array
+    * @return all the permutation
+    * 
+    * use backtracking
+    * stop point: list.size() == nums.length result.add()
+    * for iterate nums
+    *   if(!used[])
+    *       used[] =ture;//current is used
+    *       temp.add()
+    *       void dfs(boolean[] used, nums, templist, result)
+    *       temp.remove(size-1)
+    *       used[]=false;//reverse
+    * 
+    **/
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtracking(result, new ArrayList<>(), nums, new boolean[nums.length]);
+        if(nums == null || nums.length == 0){return result;}
+        dfs(result, new ArrayList<>(), new boolean[nums.length], nums);
         return result;
-    }  
+    }
     
-    public void backtracking(List<List<Integer>> result, List<Integer> tempList, int[] nums, boolean[] used){
+    public void dfs(List<List<Integer>> result, List<Integer> tempList, boolean[] used, int[] nums){
         if(tempList.size() == nums.length){
             result.add(new ArrayList<>(tempList));
             return;
         }
         
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i< nums.length; i++){
             if(!used[i]){
-                used[i]=true;
+                used[i] = true;
                 tempList.add(nums[i]);
-                backtracking(result, tempList, nums, used);
+                dfs(result, tempList, used, nums);
                 tempList.remove(tempList.size()-1);
-                used[i]=false;
+                used[i] = false;
             }
         }
     }
-    
-        /*
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums.length == 0) return result;
-
-        List<Integer> inResult = new ArrayList<>();
-        inResult.add(nums[0]);
-        result.add(inResult);
-        //for result
-        for(int i = 1; i<nums.length ; i++){
-            List<List<Integer>> tempResult = new ArrayList<>();
-            //for current tempinResult
-            for(int j = 0; j<=i; j++){
-                for(List<Integer> loopList: result){
-                    List<Integer> tempinResult = new ArrayList<>(loopList);
-                    tempinResult.add(j,nums[i]);
-                    tempResult.add(tempinResult);
-                }
-            }
-            result = tempResult;
-        }
-
-        return result;
-    }*/
-
 }
