fa816f962a2b950b9026da53bba5df36105cd955
libralmy <libralmy1003@gmail.com>
2016-09-21 23:47:46 -0400

    Update 101_Symmetric Tree.java

--- 101_Symmetric Tree.java
+++ 101_Symmetric Tree.java
@@ -1,89 +1,72 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
- 
+
  /**
- * @param root a given tree
- * @return boolean find it is Symmetric tree or not
+ * BFS
+ * using queue as temp storage
+ * conner case: root == null, l =null & | r = null
+ * push l and r into queue before while(!queue.isEmpty())
+ * poll() get l and r
+ * if(both l.l and r.r not null) push them
+ * else one of is null return false
+ * if(both l.r and r.l not null) push them
+ * else one of is null return false
  * 
- * DFS:
- * isSameTreehelper(root.left, root.right)
- * if(left==null && right ==null) return true
- * if(one if is null) return false
- * return(l.val == r.val)&& l.l, r.r && l.r, r,l
- * 
- * Iteration:BFS
- * use queue to be the temp storage
- * 
- * queue.add(left)
- * queue.add(right)
- * 
- * NO forloop for size!!
- * two general conditions
- * a l.l!=null && r.r!=null
- * b l.r!=null && r.l!=null
+ * DFS:(left, right)
+ * if(root) return true
+ * return dfs(left, right)
+ * if(both null) return true
+ * if(one of is null) return false
+ * return l.val = r.val && dfs(l.l, r.r) && dfs(l.r, r.l)
  **/
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return dfs(root.left, root.right);
+    }
+    
+    public boolean dfs(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        
+        return (left.val == right.val)&& dfs(left.left, right.right) && dfs(left.right, right.left); 
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+
         if(root == null){
             return true;
-        }else if(root.left == null && root.right == null){
+        }else if(root.left ==null && root.right ==null){
             return true;
         }else if(root.left == null || root.right == null){
             return false;
         }
         Queue<TreeNode> queue = new LinkedList<>();
-        
-        queue.add(root.left);
-        queue.add(root.right);
+        queue.offer(root.left);
+        queue.offer(root.right);
         
         while(!queue.isEmpty()){
-
-                TreeNode l = queue.poll();
-                TreeNode r = queue.poll();
-                if(l.val != r.val){ return false;}
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if(left.val != right.val){ return false;}
+            if(left.left != null && right.right != null){
+                queue.offer(left.left);
+                queue.offer(right.right);
+            }else if((left.left != null && right.right == null) || (left.left == null && right.right != null) ){
+                return false;
+            }
+            
+            if(left.right != null && right.left != null){
                 
-                if(l.left != null && r.right != null){
-                    queue.add(l.left);
-                    queue.add(r.right);
-                }else if((l.left != null && r.right == null) || (l.left == null && r.right != null)){
-                    return false;
-                }
-                
-                if(l.right != null && r.left != null){
-                    queue.add(l.right);
-                    queue.add(r.left);
-                }else if((l.right == null && r.left != null) || (l.right != null && r.left == null)){
-                    return false;
-                }
-
+                queue.offer(right.left);
+                queue.offer(left.right);
+            }else if((left.right != null && right.left == null) || (left.right == null && right.left != null) ){
+                return false;
+            }
         }
-        
         return true;
-        
+
     }
-    
-    /*
-    public boolean isSymmetric(TreeNode root) {
-        if(root == null){
-            return true;
-        }
-        return helper(root.left, root.right);
-    }
-    
-    public boolean helper(TreeNode left, TreeNode right){
-        if(left == null && right == null){
-            return left == right;
-        }
-        if(left == null || right == null){
-            return false;
-        }
-        return (left.val == right.val)&& helper(left.left, right.right) && helper(left.right, right.left);
-    }*/
 }
