4e066ff18b63a9254585f1d337864693cd52884f
libralmy <libralmy1003@gmail.com>
2016-09-26 14:17:18 -0400

    Update 000_Singleton.java

--- 000_Singleton.java
+++ 000_Singleton.java
@@ -1,3 +1,10 @@
+/**
+*In this example SingleSpoon class holds one instance of SingleSpoon in "private static SingleSpoon theSpoon;". 
+The SingleSpoon class determines the spoons availability using "private static boolean theSpoonIsAvailable = true;" 
+The first time SingleSpoon.getTheSpoon() is called it creates an instance of a SingleSpoon. 
+The SingleSpoon can not be distributed again until it is returned with SingleSpoon.returnTheSpoon(). 
+***/
+
 public class SingleSpoon 
 {  
    private Soup soupLastUsedWith;
@@ -41,4 +48,43 @@ public class SingleSpoon
    public void cleanSpoon() {
        this.setSoupLastUsedWith(null);
    }   
+}
+
+
+class TestSingleSpoon {
+    
+   public static void main(String[] args) {
+       System.out.println("First person getting the spoon");
+       SingleSpoon spoonForFirstPerson = 
+         SingleSpoon.getTheSpoon();
+       if (spoonForFirstPerson != null)
+           System.out.println(spoonForFirstPerson);
+       else
+           System.out.println("No spoon was available");
+       
+       System.out.println("");
+       
+       System.out.println("Second person getting a spoon");
+       SingleSpoon spoonForSecondPerson = SingleSpoon.getTheSpoon();
+       if (spoonForSecondPerson != null)
+           System.out.println(spoonForSecondPerson);
+       else
+           System.out.println("No spoon was available");
+       
+       System.out.println("");
+       
+       System.out.println("First person returning the spoon");
+       spoonForFirstPerson.returnTheSpoon();       
+       spoonForFirstPerson = null;
+       System.out.println("The spoon was returned");
+       
+       System.out.println("");
+       
+       System.out.println("Second person getting a spoon");
+       spoonForSecondPerson = SingleSpoon.getTheSpoon();
+       if (spoonForSecondPerson != null)
+           System.out.println(spoonForSecondPerson);
+       else
+           System.out.println("No spoon was available");
+   }
 }
