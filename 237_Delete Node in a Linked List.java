44bc1c4b086d18c2c9cc6b7505db0bb4a751632b
libralmy <libralmy1003@gmail.com>
2016-09-28 21:59:44 -0400

    Create 237_Delete Node in a Linked List.java

---
+++ 237_Delete Node in a Linked List.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if(node == null){
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+        node = null;
+    }
+}
