b3dd7759b67266121176089f51cb989ff0c6e4ec
libralmy <libralmy1003@gmail.com>
2016-10-12 00:39:10 -0400

    Update Convert a given Binary Tree to Doubly Linked List.java

--- Convert a given Binary Tree to Doubly Linked List.java
+++ Convert a given Binary Tree to Doubly Linked List.java
@@ -113,3 +113,26 @@ class BinaryTree
         tree.printList(head);
     }
 }
+///////////////////////////////////////////////////////
+	public static void convert(TreeNode root, TreeNode[] pre) {
+		if (root == null) {
+			return;
+		}
+		
+		convert(root.left, pre);
+		root.left = pre[0];
+		if (pre[0] != null) {
+			pre[0].right = root;
+		}
+		pre[0] = root;
+		convert(root.right, pre);
+	}
+	
+	public static TreeNode cc(TreeNode root) {
+		TreeNode[] pre = new TreeNode[1];
+		convert(root, pre);
+		while (root.left != null) {
+			root = root.left;
+		}
+		return root;
+	}
