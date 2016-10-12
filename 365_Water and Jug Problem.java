febe99d25b40d45a54b5f9bc86f106dd887a4406
libralmy <libralmy1003@gmail.com>
2016-10-11 22:50:31 -0400

    Update 365_Water and Jug Problem.java

--- 365_Water and Jug Problem.java
+++ 365_Water and Jug Problem.java
@@ -1,1 +1,19 @@
 
+    /**
+    * conner case: x+y==z, x==z, y==z, z==0 return true
+    * ax+by: diff between x and y z%diff == 0
+    **/
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(z == 0 || x+y == z || x == z || y == z) return true;
+        if(z > x +y ) return false;
+        return z%diff(x,y) == 0;
+    }
+    
+    public int diff(int x, int y){
+        while(y != 0){
+            int temp = y;
+            y = x%y;
+            x = temp;
+        }
+        return x;
+    }
