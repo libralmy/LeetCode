dfb57794201a4ce0685d4080519d14ee1035e1ce
libralmy <libralmy1003@gmail.com>
2016-10-16 18:05:42 -0400

    Update 283_Move Zeroes.java

--- 283_Move Zeroes.java
+++ 283_Move Zeroes.java
@@ -1,1 +1,24 @@
 
+/**
+* noZ present the index of non zero digit
+* noZ++ when num!=0
+* and then fill in 0 for rest of num
+**/
+public class Solution {
+    public static void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0){return;}
+        int noZ = 0;
+        for(int num: nums){
+            if(num != 0){
+                nums[noZ++] = num; 
+            }
+        }
+        
+        while(noZ < nums.length){
+            nums[noZ++] = 0;
+        }
+
+    }
+    
+    
+}
