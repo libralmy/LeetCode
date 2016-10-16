3e720646f83f2dd7c38103166dffd25321438451
libralmy <libralmy1003@gmail.com>
2016-10-15 20:20:48 -0400

    Update 348_Design Tic-Tac-Toe.java

--- 348_Design Tic-Tac-Toe.java
+++ 348_Design Tic-Tac-Toe.java
@@ -1,1 +1,59 @@
 
+public class TicTacToe {
+    /**
+    * @ row[n] counter for each row 
+    * @ col[n] counter for each col
+    * @ diagonal counter from upper left to bottom right
+    * @ antidiagonal counter from uper right to bottom left
+    * @ return who win the game
+    **/
+    public int[] rows;
+    public int[] cols;
+    public int diagonal;
+    public int antidiagonal;
+    
+    
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+
+        int toAdd = player == 1? 1 : -1;
+        
+        rows[row] += toAdd;
+        cols[col] += toAdd;
+        //upleft 
+        if(row == col) diagonal += toAdd;
+        
+        //upright
+        if(col == (cols.length - row - 1)) antidiagonal += toAdd; 
+        
+        int size = rows.length;
+        if(Math.abs(rows[row]) == size || 
+        Math.abs(cols[col]) == size || 
+        Math.abs(diagonal)== size || 
+        Math.abs(antidiagonal) == size){
+            return player;
+        }
+        return 0;
+        
+        
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
