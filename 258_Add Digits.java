cfecb7a60373ba91914aef0b2a88d3c27efd0c11
libralmy <libralmy1003@gmail.com>
2016-10-20 22:16:15 -0400

    Update 258_Add Digits.java

--- 258_Add Digits.java
+++ 258_Add Digits.java
@@ -1,1 +1,12 @@
+    /**
+    * d(n) = 0  -> 0
+    *        9  -> 9
+    *        n%9 !=0  -> n%9
+    *        n%9 ==0  -> 9
+    **/
+public class Solution {
 
+    public int addDigits(int num) {
+        return num == 0? 0:(num%9 ==0? 9: num%9);
+    }
+}
