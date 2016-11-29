a646772de3c0895155cb7454afa5bdd4d87d15f1
libralmy <libralmy1003@gmail.com>
2016-11-29 00:13:25 -0500

    Update and rename 235_Lowest Common Ancestor of a Binary Search Tree to 235_Lowest Common Ancestor of a Binary Search Tree.java

--- 235_Lowest Common Ancestor of a Binary Search Tree.java
+++ 235_Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,7 +1,15 @@
    /**BFT left always smaller than right
    so if p and q are smaller than root, then go deep to find LCA in left subtree, right side are same**/
    
-   
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) return null;
+        if(root.val<Math.min(p.val,q.val)) return lowestCommonAncestor(root.right,p,q);
+        if(root.val>Math.max(p.val,q.val)) return lowestCommonAncestor(root.left,p,q);
+        return root;
+    }
+    
+
+    
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
         if(root == null || p == root || q == root){return root;}
