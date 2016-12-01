45b13460083c65a925cc8ca297ecce4956b9eadc
libralmy <libralmy1003@gmail.com>
2016-11-30 22:20:53 -0500

    Update 127_Word_Ladder.java

--- 127_Word_Ladder.java
+++ 127_Word_Ladder.java
@@ -1,57 +1,58 @@
-
-
 public class Solution {
     /**
-    * BFS
-    * add end word to the dict
-    * push beginword to queue
-    *   set return count+1 when current poll() = endword
-    *   covert the word poll from queue to charArray and change every char,
-    *   then check if it is in the dict and does not = current poll()
-    *   if it is, add it into queue, remove form dict for avoding duplicate
-    *   count++
-    * return 0;
+    * Basically I keep two sets of words, one set reached that represents the borders that have been reached with "distance" steps; 
+    another set wordDict that has not been reached. 
+    In the while loop, for each word in the reached set, I give all variations and check if it matches anything from wordDict, 
+    if it has a match, I add that word into toAdd set, which will be my "reached" set in the next loop, 
+    and remove the word from wordDict because I already reached it in this step. 
+    And at the end of while loop, I check the size of toAdd, 
+    which means that if I can't reach any new String from wordDict, I won't be able to reach the endWord, 
+    then just return 0. Finally if the endWord is in reached set, I return the current steps "distance".
+
+    The idea is that reached always contain only the ones we just reached in the last step, 
+    and wordDict always contain the ones that haven't been reached. 
+    This is pretty much what Dijkstra's algorithm does, or you can see this as some variation of BFS.
+
+    ps: I get TLE at the first two submissions, because when I check if wordDict has any matches with reached set, 
+    I use two for loops and determine if any pair of words differ by one. 
+    That's a huge slow-down because it'll takes m (size of reached) * n (size of wordDict) * l (length of words) time, 
+    while in this solution, it takes 26 * l * m time. So when n is huge, this solution will be (n/26) times faster.
     **/
-    /**
-     * @param beginWord a string start to trace
-     * @param endWord a string end the trace
-     * @return the shortest distance between the two words
-     *
-     * add end word into wordlist
-     * S1 BFS: count=0 use queue to be the temporary storage for the path of word tracking
-     * S2 BFS: count=1(when reach endword, while stoped) use HashSet iterated and addin as the temporary storage for avoiding to check duplicates word in wordList
-     *      if(currChecked.size() == 0) return 0; and put lastChecked = currChecked, len++
-     * **/
-     
-     
-
-    public static int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        if(wordList.isEmpty()){return 0;}
-
-        Set<String> lastChecked = new HashSet<>();
-        lastChecked.add(beginWord);
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if(beginWord == null || endWord == null || wordList.isEmpty()){
+            return 0;
+        }
+        int counter = 1;
+        Set<String> latestList = new HashSet<>();
+        latestList.add(beginWord);
         wordList.add(endWord);
-        int len = 1;
-
-        while(!lastChecked.contains(endWord)){
-            Set<String> currChecked = new HashSet<>();
-            for(String tempStr : lastChecked){
-                for(int i = 0; i < tempStr.length(); i++){
-                    char[] tempStrCharArry = tempStr.toCharArray();
+        
+        while(!latestList.contains(endWord)){
+            Set<String> currentList = new HashSet<>();
+            for(String str : latestList){
+                
+                for(int i = 0; i < str.length(); i++){
+                    char[] charArr= str.toCharArray();
+                    
                     for(char ch = 'a'; ch <= 'z'; ch++){
-                        tempStrCharArry[i] = ch;
-                        String tempStrConvert = String.valueOf(tempStrCharArry);
-                        if(wordList.contains(tempStrConvert)){
-                            currChecked.add(tempStrConvert);
-                            wordList.remove(tempStrConvert);
+                        charArr[i] = ch;
+                        String tempStr = String.valueOf(charArr);
+                        if(wordList.contains(tempStr) && !tempStr.equals(str)){
+                            currentList.add(tempStr);
+                            wordList.remove(tempStr);
                         }
                     }
                 }
             }
-
-            if(currChecked.size() == 0) return 0;
-            lastChecked = currChecked;
-            len++;
+            
+            if(currentList.size() == 0){return 0;}
+            latestList = currentList;
+            counter++;
+            
         }
-        return len;
+        
+        return counter;
     }
+    
+    
+}
