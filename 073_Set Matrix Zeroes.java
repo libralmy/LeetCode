2bf73ebd2ac2bd37f04cc5bab55a4e74e0952331
libralmy <libralmy1003@gmail.com>
2016-12-04 02:28:39 -0500

    Update 073_Set Matrix Zeroes.java

--- 073_Set Matrix Zeroes.java
+++ 073_Set Matrix Zeroes.java
@@ -1,1 +1,27 @@
 
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        if(matrix == null || matrix.length == 0){return;}
+        boolean[] rZero = new boolean[matrix.length];
+        boolean[] cZero = new boolean[matrix[0].length];
+        
+        for(int r = 0; r < matrix.length; r++){
+            for(int c = 0; c < matrix[0].length; c++){
+                if(matrix[r][c] == 0){
+                    rZero[r] = true;
+                    cZero[c] = true;
+                }
+            }
+        }
+
+        for(int r = 0; r < matrix.length; r++){
+            for(int c = 0; c < matrix[0].length; c++){
+                if(rZero[r] || cZero[c]){
+                    matrix[r][c] = 0;
+                    
+                }
+            }
+        }
+
+    }
+}
