e4a716e24d9f22282fbaf8dccec385988d5ccc47
libralmy <libralmy1003@gmail.com>
2017-01-05 23:35:02 -0500

    Update 414_Third Maximum Number.java

--- 414_Third Maximum Number.java
+++ 414_Third Maximum Number.java
@@ -1,4 +1,49 @@
+/**
+*long small, mid, max = Long.MIN_VALUE
+if count >= 3 return small, else return max;
+***/
+public class Solution {
     public int thirdMax(int[] nums) {
+        if(nums == null || nums.length == 0){return 0;}
+        
+        long small = Long.MIN_VALUE,mid = Long.MIN_VALUE, max = Long.MIN_VALUE;
+        int count = 0;
+        
+        for(int n : nums){
+            if(n == max || n == mid){continue;}
+            
+            if(n > max){
+                
+                small = mid;
+                mid = max;
+                max = n;
+                count++;
+            }else if(n > mid){
+                small = mid;
+                mid = n;
+                count++;
+                
+            }else if(n >= small){
+                small = n;
+                count++;
+            }
+            
+            
+        }
+        
+        if(count >= 3){
+            return (int)small;
+        }else{
+            return (int)max;
+        }
+        
+    }
+}    
+
+
+
+
+public int thirdMax(int[] nums) {
         if(nums == null || nums.length == 0){return 0;}
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(4);
@@ -22,40 +67,3 @@
         return pq.peek();
     }
 
-public int thirdMax(int[] nums) {
-        int max, mid, small, count;
-        max = mid = small = Integer.MIN_VALUE;
-        count = 0;  //Count how many top elements have been found.
-
-        for( int x: nums) {
-            //Skip loop if max or mid elements are duplicate. The purpose is for avoiding right shift.
-            if( x == max || x == mid ) {
-                continue;
-            }
-
-            if (x > max) {
-                //right shift
-                small = mid;
-                mid = max;
-
-                max = x;
-                count++;
-            } else if( x > mid) {
-                //right shift
-                small = mid;
-
-                mid = x;
-                count++;
-            } else if ( x >= small) { //if small duplicated, that's find, there's no shift and need to increase count.
-                small = x;
-                count++;
-            }
-        }
-
-        //"count" is used for checking whether found top 3 maximum elements.
-        if( count >= 3) { 
-            return small;
-        } else {
-            return max;
-        }
-    }
