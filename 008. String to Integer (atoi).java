68d640e7846065544d1e0af951542695560fc996
libralmy <libralmy1003@gmail.com>
2017-01-07 19:06:56 -0500

    Update 008. String to Integer (atoi).java

--- 008. String to Integer (atoi).java
+++ 008. String to Integer (atoi).java
@@ -4,40 +4,45 @@
     * @return convert string to integer
     * conner case: flag of negative; isDigit; overflow;
     * trim();
-    * record the first elem is negative sign or not
-    * iterate str.char() from start
-    *   if(!Character.isDigit(temp)) return return (int)sum * sign;
-    *   char - '0' 
-    *   sum*10 + char - '0'
-    *   greater than MAX, smaller than MIN
-    **/
+/**
+* sign and start point
+* long sum, in number range, or out of Integer bound
+**/
+
 public class Solution {
     public int myAtoi(String str) {
         if(str == null || str.length() == 0){return 0;}
-        str = str.trim();
-        long sum = 0; 
-        int start = 0, sign = 1;
-        if(str.charAt(0) == '+'){
+        //sign
+        int sign = 1, start = 0;
+        
+        char[]digit = str.trim().toCharArray();
+        
+        if(digit[0] == '+'){
             sign = 1;
             start = 1;
-        }else if(str.charAt(0) == '-'){
+        }else if(digit[0] == '-'){
             sign = -1;
             start = 1;
         }
         
-        for(int i = start ; i < str.length(); i++){
-            char temp = str.charAt(i);
-            if(!Character.isDigit(temp)){
-                return (int)sum * sign;
-            }
-            sum = sum*10 + (temp-'0');
+        long sum = 0;
+        
+        for(int i = start; i < digit.length ; i++){
             
-            if(sign == 1 && sum >= Integer.MAX_VALUE){
+            if(digit[i] >= '0' && digit[i] <= '9'){
+                sum = sum*10 + Character.getNumericValue(digit[i]);
+            }else{
+                return (int)sum*sign;
+            }
+            
+            if(sum >= Integer.MAX_VALUE && sign == 1){
                 return Integer.MAX_VALUE;
             }else if(sign == -1 && sum * sign <= Integer.MIN_VALUE){
                 return Integer.MIN_VALUE;
+                
             }
         }
-        return (int)sum * sign;
+        
+        return (int)sum*sign;
     }
 }
