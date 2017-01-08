1f191993218c3e3f971aff6898467ae54efe05f0
libralmy <libralmy1003@gmail.com>
2017-01-08 00:24:38 -0500

    Update 119_Pascal's Triangle II.java

--- 119_Pascal's Triangle II.java
+++ 119_Pascal's Triangle II.java
@@ -1,12 +1,22 @@
+/**
+* list as the output, so Integer[rowIndex+1] result[0]=1
+* result[j] += result[j-1];
+**/
+
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
-      Integer[] result =  new Integer[rowIndex + 1];
-      Arrays.fill(result, 0);
-      result[0] = 1;
-      for(int i = 1; i < rowIndex + 1; i++)
-        for(int j = i; j >= 1; j--)
-          result[j] += result[j - 1];
-      return Arrays.asList(result);
+        Integer[] result = new Integer[rowIndex+1];
+        Arrays.fill(result, 0);
+        if(rowIndex < 0){return Arrays.asList(result); }
         
+        result[0] = 1;
+        for(int i = 1; i <= rowIndex; i++){
+            for(int j = i; j >= 1; j--){
+                result[j] += result[j-1];
+            }
+        }
+        
+        
+        return Arrays.asList(result);
     }
 }
