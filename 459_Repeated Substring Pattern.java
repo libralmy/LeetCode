a082f414a7ffeca6734d90ecaaed663990edd6f3
libralmy <libralmy1003@gmail.com>
2017-01-08 01:56:42 -0500

    Update 459_Repeated Substring Pattern.java

--- 459_Repeated Substring Pattern.java
+++ 459_Repeated Substring Pattern.java
@@ -1,28 +1,28 @@
 public class Solution {
     /**
-    * for getting the repeat substring, that means it can be devided into same length
-    * so the cursor start from halfway and without remain at least, decreaseing 
-    * ---|------ 
-    * for| while->checking the rest of string has same repeat pattern or not
+    * i = strLength/2, if(strLength % i == 0), divisor = i, 
+    * temp.equals(str.substring(divisor, divisor+i)), divisor == strLength
     **/
     public boolean repeatedSubstringPattern(String str) {
         if(str == null){
             return false;
         }
-        int strLength = str.length();
-        for(int i = strLength/2; i > 0; i--){
-            if(strLength % i == 0){
-                String tempstr = str.substring(0, i);
+        int l = str.length();
+        
+        for(int i = l/2; i >0; i--){
+            if(l % i == 0){
+                String temp = str.substring(0, i);
                 int divisor = i;
-                while(divisor < strLength){
-                    if(str.substring(divisor, divisor+i).equals(tempstr)){
-                       divisor += i; 
+                while(divisor < l){
+                    if(temp.equals(str.substring(divisor, divisor+i))){
+                        divisor += i;
                     }else{
                         break;
                     }
+                    
                 }
                 
-                if(divisor == strLength){return true;}
+                if(divisor == l){return true;}
             }
         }
         
