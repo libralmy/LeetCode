94e7342bb624e7cfa14b1d90f625f87de462872d
libralmy <libralmy1003@gmail.com>
2017-01-08 00:51:27 -0500

    Update 396_Rotate Function.java

--- 396_Rotate Function.java
+++ 396_Rotate Function.java
@@ -1,26 +1,27 @@
 /**
 * f0 = [0]*0 + [1]*1 + [2]*2 + [3]*3
-* f1 = [0]*1 + [1]*2 + [2]*3 + [3]*0
+* f1 =         [0]*1 + [1]*2 + [2]*3 + [3]*0
+* f2 =                 [0]*2 + [1]*3 + [2]*0 + [3]*1
 * f0+ sum = [0]1 [1]2 [2]3 [3]4
-* f1 = f0 + sum - n*[n-1]
+* fi = fi-1 + sum - n*[n-i]
+* fi=>  for i=1 i <= n
 **/
 
 public class Solution {
     public int maxRotateFunction(int[] A) {
         if(A == null || A.length == 0){return 0;}
         int f0 = 0, sum = 0;
-        for(int i = 0; i < A.length; i++){
-            f0 += A[i]*i;
+        int n = A.length;
+        for(int i = 0; i < n; i++){
+            f0 += i*A[i];
             sum += A[i];
         }
         
         int max = f0, fi = f0;
-        for(int i = 1; i < A.length; i++){
-            fi += sum;
-            fi -= A.length*A[A.length-i];
-            
-            max = Math.max(fi, max);
-            
+        
+        for(int i = 1; i <= n ; i++){
+            fi = fi + sum - n*A[n-i];
+            if(fi > max){max = fi;}
         }
         
         return max;
