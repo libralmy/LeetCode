a12bc0a05f99b687fbb36d6d6b8f69d490f19ea1
libralmy <libralmy1003@gmail.com>
2017-01-11 00:12:57 -0500

    Update 033_Search in Rotated Sorted Array.java

--- 033_Search in Rotated Sorted Array.java
+++ 033_Search in Rotated Sorted Array.java
@@ -15,6 +15,39 @@
     * else fall in right bound ->
     *    [mid]< target <=[right] left = mid+1
     **/
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length ==0){ return -1;}
+        
+        int start = 0, end = nums.length-1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) return mid;
+            
+            if(nums[mid] < nums[start]){//right bound
+                if(nums[mid] < target && nums[end] >= target){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+                
+            }else{//left bound
+                
+                if(nums[mid] > target && nums[start] <= target){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+        }
+        
+        return -1;
+        
+    }
+}
+
 public class Solution {
     public int search(int[] nums, int target) {
         if(nums == null || nums.length == 0) return -1;
