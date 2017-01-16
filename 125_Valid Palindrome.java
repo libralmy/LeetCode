1b5f7b220b91ef7f8c99376d058948906ece4116
libralmy <libralmy1003@gmail.com>
2017-01-15 23:49:23 -0500

    Update and rename 125_Valid Palindrome to 125_Valid Palindrome.java

--- 125_Valid Palindrome.java
+++ 125_Valid Palindrome.java
@@ -2,7 +2,10 @@
      * conner case s.length()<= 1
      * convert string to all lowercase
      * two pointer to compare begin and last
-     * if it is not char or digit, ignore it !Character.isLetterorDigit**/
+     * if it is not char or digit, ignore it !Character.isLetterorDigit(left), (right), !=, else++
+     **/
+     
+     
         public static boolean isPalindrome(String s) {
             if(s.length() <= 1) return true;
             int left = 0, right = s.length()-1;
