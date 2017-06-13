fa84ec2021355a003055a6a7936118a12565d4af
libralmy <libralmy1003@gmail.com>
2017-06-12 23:13:24 -0400

    Create 281_Zigzag Iterator.java

---
+++ 281_Zigzag Iterator.java
@@ -0,0 +1,29 @@
+public class ZigzagIterator {
+
+    Queue<Iterator> q;
+    
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+       q = new LinkedList<>();
+       
+       if(!v1.isEmpty()){q.offer(v1.iterator());}
+       if(!v2.isEmpty()){q.offer(v2.iterator());}
+    }
+
+    public int next() {
+        Iterator itr = q.poll();
+        int result = (int)itr.next();
+        if(itr.hasNext()) q.offer(itr);
+        
+        return result;
+    }
+
+    public boolean hasNext() {
+        return q.peek() != null;
+    }
+}
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
