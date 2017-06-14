36d4eeee056b8a3c5b0d1956085b688d70554c63
libralmy <libralmy1003@gmail.com>
2017-06-13 23:24:31 -0400

    Create 163_Missing Ranges.java

---
+++ 163_Missing Ranges.java
@@ -0,0 +1,81 @@
+/**
+* @param nums a given int array that missing range
+* @param lower a given int lower bound
+* @param upper a given int upper bound
+* @output a list of string that include all missing range
+* use stringbuilder to record each gap of range
+**/
+
+public class Solution {
+   
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> result = new ArrayList<>();
+
+        int next = lower;
+        
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i] > upper) result.add(getRange(next, upper));
+            if(nums[i] == upper) result.add(getRange(next, nums[i]-1));
+            
+            if(nums[i] < next || (i>0 && nums[i] == nums[i-1])){ continue;}
+            
+            if(nums[i] == next){ 
+                next++;
+                continue;
+            }
+            
+            result.add(getRange(next, nums[i]-1));
+            next = nums[i]+1;
+        }
+        
+        if(next <= upper){result.add(getRange(next, upper));}
+        
+        return result;
+    }
+    
+    public String getRange(int a, int b){
+        StringBuilder sb = new StringBuilder();
+        
+        return a == b? sb.append(a).toString(): sb.append(a).append("->").append(b).toString();
+        //return a == b? String.valueOf(a) : String.format("%d->%d",a,b);
+        
+    }
+    
+    
+ public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+    List<String> list = new ArrayList<String>();
+    if(nums == null) return list;
+    int n = nums.length;
+    
+    for(int i = 0; i <= n; i++) {
+        int lt =0, gt=0;
+        
+        if(i==0)
+            lt = lower;
+        else
+            if(nums[i-1]==Integer.MAX_VALUE)
+                break;
+            else
+                lt = nums[i-1]+1;
+        
+        if(i==n)
+            gt = upper;
+        else
+            if(nums[i]==Integer.MIN_VALUE)
+                continue;
+            else
+                gt = nums[i]-1;
+
+        addRange(list, lt, gt);
+    }
+    return list;
+}
+
+private void addRange(List<String> list, int lo, int hi) {
+    if(lo > hi) return;
+    else if(lo == hi) list.add(String.valueOf(lo));
+    else list.add(lo + "->" + hi);
+}
+    
+    
+}
