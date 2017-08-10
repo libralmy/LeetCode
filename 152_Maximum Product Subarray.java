57109ffa8df016351adf669bc334e44dbd1dd326
libralmy <libralmy1003@gmail.com>
2017-08-09 23:06:00 -0400

    Update 152_Maximum Product Subarray.java

--- 152_Maximum Product Subarray.java
+++ 152_Maximum Product Subarray.java
@@ -1,4 +1,41 @@
+public class Solution {
+    /*
+     * @param nums: An array of integers
+     * @return: An integer
+     solution: if a[i] > 0, f[i] = Math.max(f[i-1]*a[i], a[i])
+                            f[i] = Math.max(g[i-1]*a[i], a[i])
+     if a[i] < 0, g[i] = Math.min(g[i-1]*a[i], a[i])
+                    g[i] = Math.min(f[i-1]*a[i], a[i])
+    then iterate f[i] to get the max
+     */
+    public int maxProduct(int[] nums) {
+        // write your code here
 
+        int n = nums.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+        f[0] = nums[0];
+        g[0] = nums[0];
+        
+
+        for(int i = 1; i < n; i++){
+            f[i] = nums[i];
+            f[i] = Math.max(f[i-1] * nums[i], f[i]);
+            f[i] = Math.max(g[i-1] * nums[i], f[i]);
+            
+            g[i] = nums[i];
+            g[i] = Math.min(g[i-1] * nums[i], g[i]);
+            g[i] = Math.min(f[i-1] * nums[i], g[i]);
+        }
+        
+        int result = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            result = Math.max(f[i], result);
+        }
+        
+        return result;
+    }
+}
     /**
      * @param nums given int array
      * @return max product of this array
