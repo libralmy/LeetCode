771ca3a72cfda93939206b66fd8f86f3baa340e8
libralmy <libralmy1003@gmail.com>
2017-08-15 23:44:01 -0400

    Update 063_Unique Paths II.java

--- 063_Unique Paths II.java
+++ 063_Unique Paths II.java
@@ -1,3 +1,58 @@
+public class Solution {
+    /**
+     * @param obstacleGrid: A list of lists of integers
+     * @return: An integer
+     */
+    public int uniquePathsWithObstacles(int[][] A) {
+        // write your code here
+        //f[i][j] = f[i-1][j] + f[i][j-1]
+        // [i][j] = 1 => f[i][j] = 0
+        // i=0 & j=0 f[i][j]=1
+        // i=0 f[i][j] = f[i][j-1]
+        // j=0 f[i][j] = f[i-1][j]
+        if(A == null || A.length == 0 || A[0].length == 0){return 0;}
+        
+        int m = A.length;
+        int n = A[0].length;
+        int[][] f = new int[m][n];
+
+        
+        for(int i = 0 ; i < m; i++){
+           
+            for(int j = 0; j < n; j++){
+                
+                if(A[i][j] == 1){
+                    f[i][j] = 0;
+                    continue;
+                }
+                
+                if(i == 0 && j == 0){
+                    f[i][j] = 1;
+                    continue;
+                }    
+                
+                if(i == 0){
+                    f[i][j] = f[i][j-1];
+                    continue;
+                }
+                
+                if(j == 0){
+                    f[i][j] = f[i-1][j];
+                    continue;
+                }
+                
+                f[i][j] = f[i-1][j] + f[i][j-1];
+                
+                
+            }
+        }
+        
+    
+    
+    return f[m-1][n-1];
+    }
+}
+
 /**
 * for start from(0,0)
 * if[][] = 1 then path[] = 0
