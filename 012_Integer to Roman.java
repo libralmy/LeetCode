e48e4e7163b176caa8ccf416fee729ee91f35b66
libralmy <libralmy1003@gmail.com>
2017-08-16 23:09:31 -0400

    Update 012_Integer to Roman.java

--- 012_Integer to Roman.java
+++ 012_Integer to Roman.java
@@ -1,11 +1,16 @@
 public class Solution {
     public String intToRoman(int num) {
+                // write your code here
         String M[] = {"", "M", "MM", "MMM"};
         String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
         
-        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        //return M[n/1000] + C[(n/100)%10] + X[(n/10)% 10)] + I[n%10];
+        return M[n / 1000] + C[(n / 100) % 10] + X[(n / 10) % 10] + I[n % 10];
+
+
         
     }
 }
