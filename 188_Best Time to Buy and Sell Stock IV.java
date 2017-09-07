17b4e2d65b4367e8f13a6a89ce4170b5e0e03727
libralmy <libralmy1003@gmail.com>
2017-09-06 22:25:51 -0400

    Create 188_Best Time to Buy and Sell Stock IV.java

---
+++ 188_Best Time to Buy and Sell Stock IV.java
@@ -0,0 +1,79 @@
+public class Solution {
+    /*
+     * @param K: An integer
+     * @param prices: An integer array
+     * @return: Maximum profit
+     K== 0 return 0 
+     K >= n-1 return max += p[i] - p[i-1];
+    1, 3, 5 Math.max(f[i-1][j], f[i-1][j-1] + p[i-1]-p[i-2])
+     2, 4   Math.max(f[i-1][j]+ p[i-1]-p[i-2], f[i-1][j-1])
+     answer Math.max(f[n][1], f[n][3], f[n][5])
+     */
+    public int maxProfit(int K, int[] p) {
+        int n = p.length;
+        
+        if(K == 0){
+            return 0;
+        }
+        
+        
+        if(K >= n-1){
+            int max = 0;
+            for(int i = 1; i < n; i++){
+                if( p[i] > p[i-1]){
+                    max += p[i] - p[i-1];
+                }
+               
+            }
+            return max;
+        }
+
+        int[][] f = new int[n+1][2*K+1+1];
+        
+        for(int i = 0; i <= n ; i++){
+            for(int j = 1; j <= 2*K+1; j++){
+                f[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        
+        int result = 0;
+        
+        f[0][1] = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= 2*K+1; j+=2){
+                f[i][j] = getUpdate(f[i][j], f[i-1][j], 0);
+                if(i>1 && j>1) f[i][j] = getUpdate(f[i][j], f[i-1][j-1], p[i-1]-p[i-2]);
+            }
+            
+            
+            for(int j = 2; j <= 2*K; j+=2){
+                
+                if(i>1) f[i][j] = getUpdate(f[i][j], f[i-1][j], p[i-1]-p[i-2]);
+                
+                if(j>1) f[i][j] = getUpdate(f[i][j], f[i-1][j-1], 0);
+            }
+        }
+        
+
+        for(int j = 1; j <= 2*K+1; j+=2){
+            result = getUpdate(result, f[n][j], 0);
+        }
+   
+        
+        return result;
+    }
+    
+    
+    
+    public int getUpdate(int a, int b, int delta){
+        if(b == Integer.MIN_VALUE){
+            return a;
+        }
+        
+        if(b+delta > a){
+            return b+delta;
+        }
+        
+        return a;
+    }
+}
