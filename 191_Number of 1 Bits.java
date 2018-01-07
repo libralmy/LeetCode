35e135fa7038db63c3bc6ccff1f5649525160804
libralmy <libralmy1003@gmail.com>
2018-01-06 20:35:45 -0500

    Update 191_Number of 1 Bits.java

--- 191_Number of 1 Bits.java
+++ 191_Number of 1 Bits.java
@@ -1,1 +1,15 @@
 
+//count 1 in bitwise
+//for i-> 32, count += n>>i & 1 
+//Integer.bitCounter();
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            count += (n >>i) & 1;
+        }
+        
+        return count;
+    }
+}
